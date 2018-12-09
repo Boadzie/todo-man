11686f658454b736230cbdcbca5978de4f9bd696
Boadzie Daniel <boadziedaniel43@gmail.com>
2018-12-09 20:04:51 +0000

    Added todo items and added styles

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="dashboard">
+    <h1 class="subheading grey--text">Dashboard</h1>
+    <v-container class="my-5">
+      <v-card flat class="pa-3" v-for="project in projects" :key="project.title">
+        <v-layout row wrap :class="`pa-3 project ${project.status}`">
+          <v-flex xs12 md6>
+            <div class="cation grey--text">Project Title</div>
+            <div>{{project.title}}</div>
+          </v-flex>
+           <v-flex xs6 sm4 md2>
+            <div class="cation grey--text">Person</div>
+            <div>{{project.person}}</div>
+          </v-flex>
+           <v-flex xs6 sm4 md2>
+            <div class="cation grey--text">Due by</div>
+            <div>{{project.due}}</div>
+          </v-flex>
+           <v-flex xs2 sm4 md2>
+              <div class="right">
+               <v-chip small :class="`${project.status} white--text caption my-2`">
+                 {{project.status}}
+               </v-chip>
+              </div>
+          </v-flex>
+        </v-layout>
+      </v-card>
+    </v-container>
+  </div>
+</template>
+
+<script>
+
+  export default {
+    data () {
+      return {
+        projects:[
+           {title:"Learn Data Science", person:"man san", due: "32th Jan 2019", status:"ongoing", content:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."},
+           {title:"Cool off man", person:"man san", due: "32th Jan 2019", status:"overdue", content:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."},
+           {title:"Master the cooky", person:"man san", due: "32th Jan 2019", status:"complete", content:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."},
+           {title:"Learn Vue", person:"man san", due: "32th Jan 2019", status:"ongoing", content:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."},
+           {title:"Call her now", person:"man san", due: "32th Jan 2019", status:"complete", content:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."}
+        ]
+      }
+    }
+  }
+</script>
+
+<style>
+  .project.complete{
+    border-left: 4px solid #3cd1cd;
+  }
+  .project.overdue{
+    border-left: 4px solid orange;
+  }
+  .project.ongoing{
+    border-left: 4px solid tomato;
+  }
+
+  .v-chip.complete{
+    background:  #3cd1cd;
+  }
+  .v-chip.overdue{
+    background:  orange;
+  }
+  .v-chip.ongoing{
+    background:  tomato;
+  }
+</style>
---
+++ src/views/Projects.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="projects">
+    <h1 class="subheading grey--text">Projects</h1>
+    <v-container class="my-5">
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium quos quo totam corrupti aliquid, tempora quia maxime nulla exercitationem enim quidem voluptate ullam blanditiis cum dolore eveniet voluptatibus rem ab.</p>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi placeat distinctio dolorem suscipit aspernatur error beatae, sit perferendis dolorum doloremque eveniet minus, velit nulla tempore reiciendis quod, delectus eaque alias?</p>
+    </v-container>
+  </div>
+</template>
+
+<script>
+
+  export default {
+   
+  }
+</script>
